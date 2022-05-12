b4994079c0d751c44d3ed0f7d8637c6ea2593348
milhamramadhan <ramadhanmilham354@gmail.com>
2022-05-13 06:44:01 +0700

    delete file queries + connector, dan INSER INTO di database

--- himpunan/connector.js
+++
@@ -1,6 +0,0 @@
-const dataBase = require('database.sql');
-const app = dataBase();
-const Queries = require('queries.sql');
-const app2 = Queries();
-
-console.log(app2);
--- himpunan/database.sql
+++ himpunan/database.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE Data_Pelanggan;
 
-
-
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(40),
@@ -19,3 +17,20 @@ CREATE TABLE orders(
     items_orders VARCHAR(100),
     Jumlah_orders INT NOT NULL
 )
+
+
+INSERT INTO users VALUES{
+    (1,'M. Ilham Ramadhan', 'ramadhanmilham354@gmail.com');
+    (2,'ramadhanmilham', 'killingtomore007@gmail.com');
+}
+
+INSERT INTO items VALUES{
+    (1, 'Rexona', 12500),
+    (2, 'INDOMIE', 3000),
+    (3, 'POP MIE', 5000)
+}
+
+INSERT INTO orders VALUES{
+    (1, 'Rexona', 3),
+    (2, 'POP MIE', 2)
+}
