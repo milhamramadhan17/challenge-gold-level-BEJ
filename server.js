a86a6c941af24d16e6a96d228377a0b10a619813
milhamramadhan <ramadhanmilham354@gmail.com>
2022-05-13 07:00:07 +0700

    rename folder himpunan = controllers, +file users.controller.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
-
+const usersRoutes = require('./routes/user.routes');
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/users/', usersRoutes);
+
+async function main() {
+    await app.listen(3000);
+    console.log('server on port 3000');
+}
+
+
+module.exports = app;
+
+main();
 
