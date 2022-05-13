918c438025383bf2c8df728124d0cd99ac5d9477
milhamramadhan <ramadhanmilham354@gmail.com>
2022-05-13 12:44:43 +0700

    +get users in users controller, +router di router

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -7,3 +7,13 @@ const pool = new Pool({
     port: 3000,
     database: 'Data_Pelanggan'
 });
+
+const getUsers = async (req, res) => {
+    const query = 'SELECT * FROM users';
+    const result = await pool.query(query);
+    res.status(200).json(result.rows);
+}
+
+module.exports = {
+    getUsers
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const { getUsers } = require('../controllers/users.controller');
 
-const router = Router;
+const router = Router();
 
+router.get('/', getUsers);
 
 
 
